e9d50f2e97e314830685deeb067f293210c0204e
Edward Distel <edward+git@edwarddistel.com>
2023-03-08 19:37:51 -0500

    Update selectors

--- src/strava.js
+++ src/strava.js
@@ -7,9 +7,12 @@ const puppeteer = require('puppeteer');
     try {
         const url = "https://www.strava.com/login";
         const seconds = 10;
-        // If running this in Docker:
-        // const browser = await puppeteer.launch({executablePath: 'google-chrome-unstable', args: ['--no-sandbox']});
-        const browser = await puppeteer.launch();
+        /** If running this in Docker:
+        const browser = await puppeteer.launch({executablePath: 'google-chrome-unstable', args: ['--no-sandbox']});
+        
+        To test visually, set headless to false 
+        const browser = await puppeteer.launch({args: ['--disable-web-security',], headless: false}); */
+        const browser = await puppeteer.launch({args: ['--disable-web-security',]});
         const page = await browser.newPage();
         console.log("New browser window opened");
         
@@ -26,34 +29,39 @@ const puppeteer = require('puppeteer');
         
         await page.click('#login-button');
         console.log("Clicked login");
-    
+
         // Waiting x seconds seems to make this more reliable than waiting for Network Idle or a selector
         await page.waitForTimeout(seconds * 1000);
         console.log(`Waited ${seconds} seconds`);
 
-        const notificationsButtonSelector = "button[aria-controls='notifications-list']";
-        const kudsButtonsSelector = 'div[test-id^="web-feed-entry"] button[data-testid="kudos_button"]';
+        
+        const notificationsButtonSelector = `button[class^="src-Notifications__nav-bar-button"]`;
+        const kudosButtonsSelector = `button[data-testid="kudos_button"]`;
+
+
+        if (notificationsButtonSelector) {
+            // Attempt to click notifications button directly
+            await page.click(notificationsButtonSelector);
+        }
+        
+        // An alternate method to evaluate the page...sometimes this seems to work better.
+        await page.$$eval(kudosButtonsSelector, results => {
+            return results.map(result => result.click() );
+        });      
+
 
         // Page evaluate works most of the time when trying to click buttons...but then sometimes it doesn't
         await page.evaluate(() => {
-            const notifications = document.querySelector(notificationsButtonSelector);
+            const notifications = document.querySelector(`button[class^="src-Notifications__nav-bar-button"]`);
             if (notifications) {
                 notifications.click();
             }
-            const kudosButtons = document.querySelectorAll(kudsButtonsSelector);
+            const kudosButtons = document.querySelectorAll(`button[data-testid="kudos_button"]`);
             kudosButtons.forEach((btn) => {
                 btn.click();
             });
         });
-
-        // Attempt to click notifications button directly
-        await page.click(notificationsButtonSelector);
-        
-        // An alternate method to evaluate the page...sometimes this seems to work better.
-        await page.$$eval(kudsButtonsSelector, results => {
-            return results.map(result => result.click() );
-        });      
-
+ 
         console.log("Clicked notifications button and all like buttons");
         await browser.close();
     } catch (err) {
